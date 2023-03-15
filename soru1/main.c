6b9407832a7de86e61628b15cdc299e8eb81ce92
Efecan Donmez <efecandonmez00@icloud.com>
2023-03-15 22:01:18 +0300

    soru1

---
+++ soru1/main.c
@@ -0,0 +1,15 @@
+/* Algoritmalarý analiz etmenin temel amacý, bir algoritmanýn ne kadar iyi çalýþtýðýný, verimli olduðunu veya doðru sonuçlar verdiðini ölçmek ve deðerlendirmektir. Algoritma analizi, bir algoritmanýn ne kadar hýzlý çalýþtýðýný, ne kadar bellek kullanacaðýný ve ne kadar iyi ölçeklenebileceðini de belirleyebilir.
+
+Algoritma analizi yapmak için genellikle birkaç adým izlenir. Bunlar þunlardýr:
+
+Algoritmanýn çalýþma zamanýný ve bellek kullanýmýný belirleme: Algoritmanýn ne kadar zaman ve bellek kullanacaðýný tahmin etmek için bir model oluþturulur.
+
+En kötü durum analizi: Algoritmanýn en kötü durumda ne kadar süreceði belirlenir. Bu, algoritmanýn her durumda ne kadar iyi performans gösterdiðini anlamak için önemlidir.
+
+Hesaplama karmaþýklýðý: Algoritmanýn hesaplama karmaþýklýðý, verilerin boyutuna veya iþlem sayýsýna baðlý olarak nasýl deðiþtiðini belirler.
+
+Optimalite analizi: Algoritmanýn ne kadar verimli olduðunu belirleyen bir kriter belirlenir ve algoritma bu kritere göre analiz edilir.
+
+Algoritma analizi, bir algoritmanýn performansýný ölçmek için önemli bir araçtýr. Bu analiz, algoritmalarýn daha hýzlý ve daha verimli hale getirilmesine ve verilerin daha hýzlý ve daha doðru bir þekilde iþlenmesine yardýmcý olabilir.
+
+*/
